ec33786c2102534536ffb325c2e981068ad3855f
avalserg <avalserg@gmail.com>
2026-04-15 09:49:54 +0300

    Bad Example before Abstract factory

---
+++ AbstractFactory/Program.cs
@@ -0,0 +1,42 @@
+﻿new NavigationBar(new Apple()); //render specifically for some case 
+new DropdownMenu(new Apple());
+
+public class NavigationBar
+{
+    public NavigationBar(Apple factory) => factory.CreateButton(); // we only use Apple
+}
+
+public class DropdownMenu
+{
+    public DropdownMenu(Apple factory) => factory.CreateButton(); // we only use Apple
+}
+
+public interface IUIFactory
+{
+    public Button CreateButton();
+}
+
+public class Apple : IUIFactory
+{
+    public Button CreateButton()
+    {
+        return new Button()
+        {
+            Type = "iOS Button"
+        };
+    }
+}
+public class Android : IUIFactory
+{
+    public Button CreateButton()
+    {
+        return new Button()
+        {
+            Type = "Android Button"
+        };
+    }
+}
+public class Button
+{
+    public string Type { get; set; }
+}
