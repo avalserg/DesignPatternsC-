5d45b1165d8eef181205bf46b430f336ec29c160
avalserg <avalserg@gmail.com>
2026-04-15 10:18:29 +0300

    FactoryMethod example

---
+++ FactoryMethod/Program.cs
@@ -0,0 +1,75 @@
+﻿var commonNavBar = new NavigationBar();
+var androidNavBar = new AndroidNavigationBar();
+androidNavBar.Render();
+commonNavBar.Render();
+public abstract class Element
+{
+    protected Button Button { get; private set; }
+
+    protected void Initialize()
+    {
+        Button = CreateButton();
+    }
+
+    protected abstract Button CreateButton();
+    public void Render()
+    {
+        Console.WriteLine($"Rendering {Button.Type}");
+    }
+
+}
+public class NavigationBar : Element
+{
+    public NavigationBar()
+    {
+        Initialize();
+    }
+
+    protected override Button CreateButton()
+    {
+        return new Button { Type = "DefaultButton" };
+    }
+}
+
+public class DropdownMenu : Element
+{
+    public DropdownMenu()
+    {
+        Initialize();
+    }
+
+    protected override Button CreateButton()
+    {
+        return new Button { Type = "DefaultButton" };
+    }
+}
+public class AndroidNavigationBar : Element
+{
+    public AndroidNavigationBar()
+    {
+        Initialize();
+    }
+
+    protected override Button CreateButton()
+    {
+        return new Button { Type = "Android DefaultButton" };
+    }
+}
+
+public class AndroidDropdownMenu : Element
+{
+    public AndroidDropdownMenu()
+    {
+        Initialize();
+    }
+
+    protected override Button CreateButton()
+    {
+        return new Button { Type = "Android DefaultButton" };
+    }
+}
+
+public class Button
+{
+    public string Type { get; set; }
+}
