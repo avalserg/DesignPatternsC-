645bc82bd16af1b228ea45b3a61dd205530f69b3
avalserg <avalserg@gmail.com>
2026-04-15 09:34:12 +0300

    Problem before inject factory pattern

--- Factory/Program.cs
+++ Factory/Program.cs
@@ -1,1 +1,36 @@
-﻿
+﻿new NavigationBar();
+new DropdownMenu();
+
+//Bad Example
+//We need to change Type in several places
+//if some logic changes
+public class NavigationBar
+{
+    public NavigationBar() =>
+        new Button()
+        {
+            Type = "DefaultButton" // rename to RedButton
+        };
+
+}
+
+public class DropdownMenu
+{
+    public DropdownMenu() =>
+        new Button()
+        {
+            Type = "DefaultButton" // rename to RedButton
+        };
+}
+
+public class ButtonFactory
+{
+    public static Button CreateButton()
+    {
+        return null;
+    }
+}
+public class Button
+{
+    public string Type { get; set; }
+}
