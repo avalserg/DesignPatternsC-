0825e8384fc35dc7ca75c04e1020793a472c2a87
avalserg <avalserg@gmail.com>
2026-04-16 09:38:25 +0300

    Adapter pattern

---
+++ Adapter/Program.cs
@@ -0,0 +1,35 @@
+﻿Adaptee adaptee = new Adaptee();
+ITarget target = new Adapter(adaptee);
+
+Console.WriteLine("Adaptee interface is incompatible with the client.");
+Console.WriteLine("But with adapter client can call it's method.");
+
+Console.WriteLine(target.GetRequest());
+
+public interface ITarget
+{
+    string GetRequest();
+}
+
+class Adaptee
+{
+    public string GetSpecificRequest()
+    {
+        return "Specific request.";
+    }
+}
+
+class Adapter : ITarget
+{
+    private readonly Adaptee _adaptee;
+
+    public Adapter(Adaptee adaptee)
+    {
+        _adaptee = adaptee;
+    }
+
+    public string GetRequest()
+    {
+        return $"This is '{_adaptee.GetSpecificRequest()}'";
+    }
+}
